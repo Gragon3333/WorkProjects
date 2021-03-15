4b0750deca72dd2387a8f6663a57ef2297450cbb
Viktor <evdokimov.viktor.work@mail.ru>
2021-03-15 12:30:07 +0700

    Отредактирован код

--- TaskOneNumbers.cs
+++ TaskOneNumbers.cs
@@ -5,11 +5,8 @@ namespace Test
 {
     class Program
     {
-        
-        
         static void Main(string[] args)
         {
-            
             string someText = Console.ReadLine();
             List<char> numbers = new List<char>();
             char[] textToArray = someText.ToCharArray();
@@ -17,21 +14,17 @@ namespace Test
             for (int i = 0; i < charsToASCII.Length; i++)
             {
                 charsToASCII[i] = Convert.ToInt32(textToArray[i]);
-            }
-
-            for (int i = 0; i < charsToASCII.Length; i++)
-            {
                 if (charsToASCII[i] > 47 && charsToASCII[i] < 58)
                 {
                     numbers.Add(Convert.ToChar(charsToASCII[i]));
                 }
-                
+
             }
             char[] singleNumbers = numbers.Distinct().ToArray();
             foreach (var item in singleNumbers)
             {
-                var ct = numbers.Count(cg => cg == item);
-                Console.WriteLine(item+":"+ct);
+                var count = numbers.Count(cg => cg == item);
+                Console.WriteLine(item + ":" + count);
             }
             Console.ReadKey();
         }
